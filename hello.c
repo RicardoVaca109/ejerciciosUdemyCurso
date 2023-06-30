bfdcc6ebdbb508fca89560be30f8733ad144aeb6
Ricardo <ricardo.vaca@udla.edu.ec>
2023-06-30 02:01:06 -0400

    udemyCourse

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+Para comentarios
+Multilinea
+
+*/
+int main (int argc, char *argv[])
+{
+    //estas lineas de códigos imprimen en consola texto(ej. comentario en C)
+    printf("Hello World \n"); 
+    printf ("Hola MUndo desde C \n");
+
+    return 0;
+}
