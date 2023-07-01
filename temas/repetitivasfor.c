ddae85f40d8aaa43790ca70d8b0e35adab891baf
Ricardo.Vaca <ricardo.vaca@udla.edu.ec>
2023-07-01 00:53:01 -0500

    Carpetas Curso Udemy

---
+++ temas/repetitivasfor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    for (int x = 0; x <= 10; x=x+2)
+    {
+        printf("%d\n", x);
+    }
+    
+    return 0;
+}
---
+++ temas/repetitivaswhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+    int n = 0;
+    int x = 9;
+    while (n < 10 && x < 10)
+    {
+        printf("%d\n",n);
+        n++;
+        
+    }
+    
+    return 0;
+}
